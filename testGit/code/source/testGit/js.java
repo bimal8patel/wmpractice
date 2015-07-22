295f6d45a6d590e3e0e5c0e8952fcf3a5d7263f1
mangatrai <mangatrai@users.noreply.github.com>
2015-07-21 17:57:24 -0700

    testing commit

---
+++ testGit/code/source/testGit/js.java
@@ -0,0 +1,41 @@
+package testGit;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2015-07-21 17:52:50 PDT
+// -----( ON-HOST: sagbase.localdomain
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class js
+
+{
+	// ---( internal utility methods )---
+
+	final static js _instance = new js();
+
+	static js _newInstance() { return new js(); }
+
+	static js _cast(Object o) { return (js)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void myJavaSvc (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(myJavaSvc)>> ---
+		// @sigtype java 3.5
+		//this is empty service
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ testGit/ns/testGit/js/myJavaSvc/java.frag
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">myJavaSvc</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">Ly90aGlzIGlzIGVtcHR5IHNlcnZpY2U=</value>
+</Values>
